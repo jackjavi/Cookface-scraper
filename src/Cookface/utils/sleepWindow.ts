39cddf130e389d50b6378383ae21405af22004de
jackjavi <jackmtembete@gmail.com>
2025-08-24 03:13:03 +0300

    update again

--- src/Cookface/utils/sleepWindow.ts
+++ src/Cookface/utils/sleepWindow.ts
@@ -6,6 +6,6 @@ export function isWithinSleepWindow(): boolean {
   return (
     (hours === 1 && minutes >= 45) || // 00:45–00:59
     (hours >= 1 && hours < 55) || // 01:00–04:59
-    (hours === 4 && minutes < 30) // 05:00–05:29
+    (hours === 2 && minutes < 30) // 05:00–05:29
   );
 }
